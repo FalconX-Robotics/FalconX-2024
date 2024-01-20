72cab8d59c16e01dda414dd29793e25776052768
William <95259563+TaylorHart02@users.noreply.github.com>
2024-01-20 14:33:32 -0800

    Shooter Command main code
    Updated Shoot Command to use Can Spark Max
    Updated Shoot Command to use Smart Dashboard
    
    -William, not Taylor
    
    Co-Authored-By: William <95259615+WilliamDeGuzman@users.noreply.github.com>

--- roboRIO-2024/src/main/java/frc/robot/Settings.java
+++ roboRIO-2024/src/main/java/frc/robot/Settings.java
@@ -4,6 +4,7 @@ import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.XboxController;
  
 public class Settings {
+    // Access Controllers
     private XboxController m_driveController, m_noteController;
     Settings (XboxController driveController, XboxController noteController){
         m_driveController = driveController;
@@ -12,19 +13,21 @@ public class Settings {
     public DriveController driveController = new DriveController();
     public NoteController noteController = new NoteController();
 
+    /** Configurations for controller centered around drivetrain repositioning */
     public class DriveController {          
-        public boolean getTurnInPlaceButton () { return m_driveController.getAButtonPressed();}
-        public double getSpeedJoystick () { return MathUtil.applyDeadband(
+        public boolean getTurnInPlaceButtonValue () { return m_driveController.getAButtonPressed();}
+        public double getSpeedJoystickValue () { return MathUtil.applyDeadband(
             m_driveController.getLeftY(), deadband);
         }
-        public double getRotationJoystick () { return MathUtil.applyDeadband( 
+        public double getRotationJoystickValue () { return MathUtil.applyDeadband( 
             m_driveController.getRightX(), deadband);
         }
         public double deadband = 0.1;
     }
+    /** Configurations for controller centered around note manipulation */
     public class NoteController {
-        public boolean getShooterButton () {return m_noteController.getAButtonPressed();}
-        public double getArmJoystick () {return MathUtil.applyDeadband( 
+        public boolean getShooterButtonValue () {return m_noteController.getAButtonPressed();}
+        public double getArmJoystickValue () {return MathUtil.applyDeadband( 
             m_noteController.getLeftY(), deadband);
         }
         public double deadband = 0.1;
