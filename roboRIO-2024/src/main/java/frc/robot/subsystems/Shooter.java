491eea12f146d81714bd62206691659667e45ab7
Max Morehead <m@xmorehead.com>
2024-01-27 20:19:37 -0800

    Simulation support for shooter
    This commit adds simulation support for the shooter. The shooter uses
    PID on board the SparkMAX. We copy the SparkMAX onboard implementation
    in Java, using documentation from Rev. We use inheritance to replace
    instances of SparkPIDController with SparkPIDControllerSim, and then
    pass velocity from the physics simulation to the PID sim each step. In
    turn, we use the PID sim to update the voltage for the physics sim.

--- roboRIO-2024/src/main/java/frc/robot/subsystems/Shooter.java
+++ roboRIO-2024/src/main/java/frc/robot/subsystems/Shooter.java
@@ -6,9 +6,16 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.SparkPIDController;
+import com.revrobotics.SparkPIDControllerSim;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.units.BaseUnits;
+import edu.wpi.first.units.Units;
+import edu.wpi.first.wpilibj.simulation.FlywheelSim;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Robot;
 import frc.robot.Constants.MotorConstants;
 
 public class Shooter extends SubsystemBase {
@@ -24,7 +31,11 @@ public class Shooter extends SubsystemBase {
   }
 
   public SparkPIDController getShooterPidController () {
-    return shooterSparkMax.getPIDController();
+    if (Robot.isSimulation()) {
+      return m_pidControllerSim;
+    } else {
+      return shooterSparkMax.getPIDController();
+    }
   }
 
   public double getShooterArmEncoderRotation() {
@@ -33,7 +44,9 @@ public class Shooter extends SubsystemBase {
 
   /** Creates a new Shooter. */
   public Shooter() {
-    //TODO: Set inverted if applicable
+    if (Robot.isSimulation()) {
+      m_pidControllerSim = new SparkPIDControllerSim(shooterSparkMax);
+    }
   }
 
   @Override
@@ -43,5 +56,28 @@ public class Shooter extends SubsystemBase {
 
     // TODO: set position conversion factor if necessary
     // This method will be called once per scheduler run
+  }
+
+  // Moment of inertia for uniform cylinder = 1/2 * m * r^2.
+  FlywheelSim shooterSim = new FlywheelSim(DCMotor.getNEO(2), 1, 
+      0.5 * BaseUnits.Mass.convertFrom(8, Units.Pounds) *
+      Math.pow(BaseUnits.Distance.convertFrom(2, Units.Inches), 2));
+  SparkPIDControllerSim m_pidControllerSim;
+
+  @Override
+  public void simulationPeriodic() {
+    SparkPIDControllerSim pidSim = (SparkPIDControllerSim) getShooterPidController();
+    // Simulate every 1ms to match PID controller implementation on Spark MAX
+    for (int i = 0; i < 20; i++) {
+      // Set the input to the physics simulation to the output of the simulated PID controller
+      shooterSim.setInputVoltage(pidSim.getVoltageOutput());
+      shooterSim.update(0.001);
+
+      // Update the velocity input to the PID controller using the physics sim
+      pidSim.setVelocity(shooterSim.getAngularVelocityRPM());
+      pidSim.update(0.001);
+    }
+    SmartDashboard.putNumber("Sim Shooter Voltage", pidSim.getVoltageOutput());
+    SmartDashboard.putNumber("Sim Shooter RPM", shooterSim.getAngularVelocityRPM());
   }
 }
