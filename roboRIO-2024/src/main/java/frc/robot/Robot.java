32155236978372e59050bda4b406ada43ba5b420
FatCactis <crawlingbeetles64@gmail.com>
2024-03-02 10:10:57 -0800

    fixed command scheduler in robot.java again

--- roboRIO-2024/src/main/java/frc/robot/Robot.java
+++ roboRIO-2024/src/main/java/frc/robot/Robot.java
@@ -50,7 +50,7 @@ public class Robot extends TimedRobot {
     // commands, running already-scheduled commands, removing finished or interrupted commands,
     // and running subsystem periodic() methods.  This must be called from the robot's periodic
     // block in order for anything in the Command-based framework to work.
-    //CommandScheduler.getInstance().run();
+    CommandScheduler.getInstance().run();
     m_robotContainer.periodic();
   }
 
