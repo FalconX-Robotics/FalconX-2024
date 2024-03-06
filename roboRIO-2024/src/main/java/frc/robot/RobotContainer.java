ae51c8020373f20717497b16c6aca7e870257457
Amelie <148516461+makofish07@users.noreply.github.com>
2024-03-05 17:38:09 -0800

    put lava into the branches where it doesnt set to Blue

--- roboRIO-2024/src/main/java/frc/robot/RobotContainer.java
+++ roboRIO-2024/src/main/java/frc/robot/RobotContainer.java
@@ -126,16 +126,17 @@ public class RobotContainer {
     );
     new Trigger(()->{return m_sensor.getNoteSensed();}).whileTrue(
       Commands.run(()->{
-      m_leds.setColor(LEDs.Color.LAVA);
       //m_vision.poseOffsetLedIndicator();
       Optional<Double> angle = m_vision.getAngleToTarget();
       if (angle.isEmpty()) {
+        m_leds.setColor(LEDs.Color.LAVA);
         DashboardHelper.putString(LogLevel.Info, "Angle alignment to target", "Target Not Present.");
       } else if(Math.abs(angle.get()) < 5) {
         m_vision.ledColor.setColor(m_vision.ledsIsAligned);
         DashboardHelper.putString(LogLevel.Info, "Angle alignment to target", "Aligned.");
                 
       } else if(Math.abs(angle.get()) > 5) {
+        m_leds.setColor(LEDs.Color.LAVA);
         DashboardHelper.putString(LogLevel.Info, "Angle alignment to target", "Unaligned.");
       }
 
